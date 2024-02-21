ebd2879f308b988c9f096b89d3a811384c383d8c
ThomasF <thomasF>
2024-02-21 09:40:46 +0100

    first commit

---
+++ src/main/java/ProjetFilRougeEquipe/ProjetFilRougeEquipe/ProjetFilRougeEquipeApplication.java
@@ -0,0 +1,13 @@
+package ProjetFilRougeEquipe.ProjetFilRougeEquipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFilRougeEquipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFilRougeEquipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjetFilRougeEquipe/ProjetFilRougeEquipe/ProjetFilRougeEquipeApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetFilRougeEquipe.ProjetFilRougeEquipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFilRougeEquipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
