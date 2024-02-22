2bb9793115e20fb4c4fbb6f05d017119e1ab8696
Séverine <92460484+SeverineVil@users.noreply.github.com>
2024-02-22 15:40:55 +0100

    saisie d'une commande ok + ajout d'une méthode de log.info

--- src/main/java/ProjetFilRougeEquipe/ProjetFilRougeEquipe/services/CommandeService.java
+++ src/main/java/ProjetFilRougeEquipe/ProjetFilRougeEquipe/services/CommandeService.java
@@ -3,7 +3,9 @@ package ProjetFilRougeEquipe.ProjetFilRougeEquipe.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ProjetFilRougeEquipe.ProjetFilRougeEquipe.entities.Client;
 import ProjetFilRougeEquipe.ProjetFilRougeEquipe.entities.Commande;
+import ProjetFilRougeEquipe.ProjetFilRougeEquipe.entities.Table;
 import ProjetFilRougeEquipe.ProjetFilRougeEquipe.repositories.CommandeRepository;
 
 @Service
@@ -19,5 +21,24 @@ public class CommandeService {
 	public Commande findCdeById(int id) {
 		return cderepo.findById(id).get();
 	}
+	
+	public Commande ouvertureCde(int id_client, int id_table) {
+		Commande newCde = new Commande();
+		Client newClient = new Client();
+		Table newTable = new Table();
+		
+		newClient.setId(id_client);
+		newTable.setId(id_table);
+		
+		newCde.setClient(newClient);
+		newCde.setTable(newTable);		
+		
+		newCde.setEtat("OPEN");
+		return cderepo.save(newCde);
+		
+	
+		
+	}
+	
 
 }
